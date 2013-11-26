36dec20d2f019252bfe7c5da504750a6be527eec
Collin J. Sutton <collin.sutton@gmail.com>
2013-11-26 01:37:44 -0800

    tests/live-test:  Reorganized, commented for better readability.

--- tests/live-test.cc
+++ tests/live-test.cc
@@ -5,7 +5,7 @@
  */
 #include <cstdio>
 #include <csignal>
-#include <set>
+#include <unordered_set>
 
 #include <crisp/comms/ProtocolNode.hh>
 #include <crisp/comms/Sensor.hh>
@@ -17,6 +17,9 @@
 #include <boost/asio/ip/tcp.hpp>
 #include <boost/asio/connect.hpp>
 
+/* ****************************************************************
+ * Help text stuff.
+ */
 #define PRINT_USAGE(stream) fprintf(stream, "Usage: %s [OPTION]... ADDRESS PORT\n", argv[0])
 
 #define HELP_TEXT "\
@@ -28,37 +31,44 @@ Options:\n\
   -h	Show this help.\n"
 
 
-using namespace crisp::comms;
+/* ****************************************************************
+ * Various types/namespaces/etc. that we'll be using.
+ */
+namespace ip = boost::asio::ip;
+typedef ip::tcp Transport;
+typedef crisp::comms::ProtocolNode<typename Transport::socket> Node;
+typedef std::unordered_set<Node*> NodeSet;
+
+/* **************************************************************** */
 
 /** Set up some generic callbacks on a ProtocolNode.
  *
  * @param node The node on which callbacks are to be applied.
  */
-template < typename _SocketType >
-void setup_callbacks(ProtocolNode<_SocketType>& node)
+void setup_callbacks(::Node& node)
 {
   node.dispatcher.sync.sent =
-    [&](ProtocolNode<_SocketType>&)
+    [&](Node&)
     {
       fprintf(stderr, "[0x%0x] \033[1;34mSYNC\033[0m out\n", THREAD_ID);
     };
 
   node.dispatcher.sync.received =
-    [&](ProtocolNode<_SocketType>&)
+    [&](Node&)
     {
       fprintf(stderr, "[0x%0x] \033[1;34mSYNC\033[0m in\n", THREAD_ID);
     };
 
 
   node.dispatcher.handshake.sent =
-    [&](ProtocolNode<_SocketType>&, const Handshake& hs)
+    [&](Node&, const Handshake& hs)
     {
       fprintf(stderr, "[0x%0x] \033[1;33mHandshake sent:\033[0m requested role \033[97m%s\033[0m\n", THREAD_ID,
 	      hs.role == NodeRole::MASTER ? "MASTER" : "SLAVE");
     };
 
   node.dispatcher.handshake.received =
-    [&](ProtocolNode<_SocketType>& _node, const Handshake& hs)
+    [&](Node& _node, const Handshake& hs)
     {
       fprintf(stderr, "[0x%0x] \033[1;33mHandshake received:\033[0m requests role \033[97m%s\033[0m\n", THREAD_ID,
 	      hs.role == NodeRole::MASTER ? "MASTER" : "SLAVE");
@@ -78,7 +88,7 @@ void setup_callbacks(ProtocolNode<_SocketType>& node)
     };
 
   node.dispatcher.handshake_response.received =
-    [&](ProtocolNode<_SocketType>& _node, const HandshakeResponse& hs)
+    [&](Node& _node, const HandshakeResponse& hs)
     {
       fprintf(stderr, "[0x%0x] \033[1;33mHandshake response received:\033[0m remote node %s role \033[97m%s\033[0m\n", THREAD_ID,
 	      hs.acknowledge == HandshakeAcknowledge::ACK ? "\033[32maccepts\033[0m" : "\033[31mrejects\033[0m",
@@ -89,7 +99,7 @@ void setup_callbacks(ProtocolNode<_SocketType>& node)
     };
 
   node.dispatcher.handshake_response.sent =
-    [&](ProtocolNode<_SocketType>&, const HandshakeResponse& hs)
+    [&](Node&, const HandshakeResponse& hs)
     {
       fprintf(stderr, "[0x%0x] \033[1;33mHandshake response sent:\033[0m %s role \033[97m%s\033[0m\n", THREAD_ID,
 	      hs.acknowledge == HandshakeAcknowledge::ACK ? "\033[32maccepting\033[0m" : "\033[31mrejecting\033[0m",
@@ -123,13 +133,16 @@ wait_for_signal()
 }
 
 
-int
-main(int argc, char* argv[])
+
+/** Main server routine; called when the program is invoked as a server.
+ */
+static void
+server_main(typename Transport::acceptor& acceptor, NodeSet& nodes)
 {
   using namespace crisp::comms::keywords; /* for _neutral, _minimum, _maximum, etc. */
-  namespace asio = boost::asio;
 
-  Configuration config;
+  /* Declare an interface configuration, and add some interesting things to it. */
+  crisp::comms::Configuration config;
   config.add_module( "drive", 2 )
     .add_input<int8_t>({ "speed", { _neutral = 0, _minimum = -127, _maximum = 127 } })
     .add_input<int8_t>({ "turn", { _neutral = 0, _minimum = -127, _maximum = 127 } });
@@ -140,15 +153,68 @@ main(int argc, char* argv[])
     .add_input<float>({ "joint1", { _minimum = -M_PI_2, _maximum = M_PI_2 }});
 
   config.modules[0]
-    .add_sensor<uint8_t>({ "drive camera", SensorType::CAMERA, { _array = true, _array_size_width = 4 } })
     .add_sensor<uint16_t>({ "front proximity", SensorType::PROXIMITY, { _minimum = 50, _maximum = 500 } })
     .add_sensor<uint16_t>({ "rear proximity", SensorType::PROXIMITY, { _minimum = 50, _maximum = 500 } });
 
 
+  acceptor.listen();		/* start listening for incomint connections. */
+
+  /* Grab the I/O coordinator service used by the connection-acceptor object. */
+  boost::asio::io_service& service ( acceptor.get_io_service() );
+
+
+  fputs("Entering `accept()` loop.\n", stderr);
+  while ( acceptor.is_open() )
+    {
+      boost::system::error_code ec;
+      boost::asio::ip::tcp::endpoint endpoint;
+
+      /* Create an unconnected socket for `accept` to initialize. */
+      Transport::socket socket ( service );
+
+      /* `accept` blocks until we receive an incoming connection or an
+	 error occurs.  */
+      acceptor.accept(socket, endpoint, ec);
+      if ( ec )
+	{
+	  fprintf(stderr, "accept: %s\n", ec.message().c_str());
+	  break;
+	}
+      else
+	{
+	  std::cerr << "Accepted connection from " << endpoint << std::endl;
+
+	  /* We've got a connection.  Create a new protocol-node on the
+	   * connection and add it to our set of connected nodes. */
+	  Node* node ( new Node(std::move(socket), NodeRole::SLAVE) );
+	  nodes.emplace(node);
+
+	  /* Set up callbacks on the node. */
+	  setup_callbacks(*node); /* standard callbacks */
+
+	  node->dispatcher.configuration_query.received = /* slave-specific callbacks */
+	    [&](Node& _node)
+	    {
+	      _node.send(Message(config));
+	    };
+
+	  /* Launch the node's worker threads. */
+	  node->launch();
+	}
+    };
+}
+
+
+/** Program entry point.  Parses the command line, and either does something
+ * useful or yells at the user. */
+int
+main(int argc, char* argv[])
+{
+
   /** User-selectable mode to run the process as.
    *
    * In SERVER mode, the process listens on the given port and address for incoming connections,
-   * and spawns a new ProtocolNode instance for each incoming connection.
+   * and spawns a new Node instance for each incoming connection.
    *
    * In CLIENT mode, the process connects to an already-running process in SERVER mode.
    */
@@ -157,6 +223,7 @@ main(int argc, char* argv[])
     SERVER,
     CLIENT
   } mode ( Mode::CLIENT );
+
 
   /* Parse user options. */
   int c;
@@ -176,21 +243,19 @@ main(int argc, char* argv[])
 	abort();
       }
 
-  if ( argc < 3 )
+  if ( argc - optind < 2 )
     {
       PRINT_USAGE(stderr);
       return 1;
     }
 
-  typedef asio::ip::tcp::socket SocketType;
-
 
   /* Parse the address and port passed via the command line, and inform the user if
-     he/she is doin' it wrong. */
+     they're doin' it wrong. */
   char* tail;
   boost::system::error_code pec;
+  ip::address address ( ip::address::from_string(argv[optind], pec) );
 
-  asio::ip::address address ( asio::ip::address::from_string(argv[optind], pec) );
   int port ( strtoul(argv[optind + 1], &tail, 0) );
 
   if ( pec )
@@ -201,109 +266,80 @@ main(int argc, char* argv[])
     { fprintf(stderr, "%s: Need integer port number.\n", argv[optind + 1]);
       return 1; }
 
-  asio::ip::tcp::endpoint target_endpoint(address, port);
+  /* Construct the target endpoint (IP address + port number) object. */
+  Transport::endpoint target_endpoint(address, port);
+
+
+  /* Asio's io_service manages all the low-level details of parceling out work
+     to IO worker threads in a thread-safe manner.  It's required for pretty
+     much any network-related function, so we'll create one here.  */
+  boost::asio::io_service service;
+
+
   if ( mode == Mode::SERVER )
     {
-      typedef std::set<ProtocolNode<SocketType> > NodeSet;
+      /* We'll want to have a list of connected protocol nodes so we can stop
+	 them later.  */
       NodeSet nodes;
 
-      asio::io_service service;
-      asio::ip::tcp::acceptor acceptor { service, target_endpoint };
+      /* Similarly, we need to have a way to close the server socket after the
+	 call to `wait_for_signal`. */
+      Transport::acceptor acceptor { service, target_endpoint };
+
 
       /* Launch our server loop in a separate thread.  */
-      std::thread
-	server_thread([&]()
-		      {
-			acceptor.listen();
-			fputs("Entering `accept()` loop.\n", stderr);
-			while ( acceptor.is_open() )
-			  {
-			    boost::system::error_code ec;
-			    asio::ip::tcp::endpoint endpoint;
+      std::thread server_thread([&]() { server_main(acceptor, nodes); });
 
-			    /* Create an unconnected socket for `accept` to initialize. */
-			    SocketType socket ( service );
-
-			    /* `accept` blocks until we receive an incoming connection or an
-			       error occurs.  */
-			    acceptor.accept(socket, endpoint, ec);
-			    if ( ec )
-			      {
-				fprintf(stderr, "accept: %s\n", ec.message().c_str());
-				break;
-			      }
-			    else
-			      {
-				std::cerr << "Accepted connection from " << endpoint << std::endl;
-
-				/* We've got a connection.  Add it to our to our set of
-				 * connected nodes, set up the callbacks we want, and
-				 * launch the node's IO threads.
-				 */
-
-				std::pair<NodeSet::iterator,bool>
-				  emplace_result ( nodes.emplace(std::move(socket), NodeRole::SLAVE) );
-
-				if ( emplace_result.second ) /* No such node already existed in
-								the set, so the new one was
-								added */
-				  {
-				    ProtocolNode<SocketType>&
-				      node ( const_cast<ProtocolNode<SocketType>&>(*(emplace_result.first)) );
-
-				    setup_callbacks(node);
-				    node.dispatcher.configuration_query.received =
-				      [&](ProtocolNode<SocketType>& _node)
-				      {
-					_node.send(Message(config));
-				      };
-
-				    node.launch();
-				  }
-			      }
-			  };
-		      });
-
-      wait_for_signal();
+      wait_for_signal();	/* twiddle our thumbs until the user gets
+				   bored. */
 
       /* FIXME: `acceptor.close()` (below) doesn't seem to properly halt... something.
-         In the mean time, just hit Ctrl+C twice to kill the program... */
+         In the mean time, you can just hit Ctrl+C twice to kill the program... */
       acceptor.close();
 
       /* Shut down all of the client nodes. */
-      for ( const ProtocolNode<SocketType>& node : nodes )
-	const_cast<ProtocolNode<SocketType>&>(node).halt();
+      for ( Node* node : const_cast<NodeSet&>(nodes) )
+	{
+	  node->halt();
+	  delete node;
+	}
+
+      server_thread.join();
     }
   else				/* mode == Mode::CLIENT */
     {
-      namespace ip = boost::asio::ip;
-
-      asio::io_service service;
-      ip::tcp::resolver resolver ( service );
-
       /* Create a socket, and try to connect it to the specified endpoint. */
       boost::system::error_code ec;
-      SocketType socket ( service );
+      Transport::socket socket ( service );
+      Transport::resolver resolver ( service );
 
-      asio::connect(socket, resolver.resolve(target_endpoint), ec);
 
-      if ( ec )
+      boost::asio::connect(socket, resolver.resolve(target_endpoint), ec);
+
+      if ( ec )			/* Didn't work! */
 	{
 	  fprintf(stderr, "connect: %s\n", ec.message().c_str());
 	  return 1;
 	}
       else
 	{
-	  ProtocolNode<SocketType> node ( std::move(socket), NodeRole::MASTER );
+	  /* We're connected.  Set up a protocol node on the socket. */
+	  Node node ( std::move(socket), NodeRole::MASTER );
+
+	  /* Set up callbacks. */
 	  setup_callbacks(node);
 	  node.dispatcher.configuration_response.received =
-	    [&](ProtocolNode<SocketType>& _node, const Configuration& config)
+	    [&](Node& _node, const Configuration& config)
 	    {
-	      /* FIXME: do something interesting here. */
+
 	    };
 
+	  /* Launch the I/O worker threads. */
 	  node.launch();
-	  node.wait();		/* wait for the node to halt. */
+
+	  wait_for_signal();	/* go get some coffee. */
+
+	  node.halt();
 	}
       
     }
