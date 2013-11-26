7f366e74662076af3af6863b2e5a33be0258a8de
Collin J. Sutton <collin.sutton@gmail.com>
2013-11-26 13:04:04 -0800

    util/PeriodicScheduler: fleshed out the interface a little more
    Probably close to being ready to implement this.

--- include/crisp/util/PeriodicScheduler.hh
+++ include/crisp/util/PeriodicScheduler.hh
@@ -30,7 +30,7 @@ namespace crisp
 				   slot. */
 
 
-	/** Reschedule the action  */
+	/** Reschedule the action by changing the slot used.  */
 	void reschedule(std::chrono::interval interval);
 
 
@@ -45,18 +45,37 @@ namespace crisp
 	void cancel();
       };
 
-      /** A regularly-occurring signal producer.
+
+      /** A regularly-occurring-signal producer.  A slot sets up a timer, and
+       * when that timer expires launches a thread to invoke all actions
+       * assigned to it; the timer is then reset and the process starts again.
+       *
+       * `Slot` manages its assigned actions through member functions `push` and
+       * `delete`.  If a previously-non-empty slot becomes empty at any time, it
+       * will cancel its timer and reset it only when it becomes non-empty
+       * again.
        */
       struct Slot
       {
 	/** Timer type used. */
 	typedef boost::asio::steady_timer Timer;
 
-	
-	Timer timer;			/**< The slot's timer. */
-	std::chrono::duration interval;	/**< Interval at which the slot's
-					   handler runs. */
-	std::set<Action> actions;	/**< Actions assigned to this slot. */
+	/** The slot's timer. */
+	Timer timer;
+
+	/** Interval at which the slot's handler runs. */
+	std::chrono::duration interval;
+
+	/** Actions assigned to this slot. */
+	std::set<Action> actions;
+
+	/** Constructor.
+	 *
+	 * @param scheduler Scheduler object that created this slot.
+	 *
+	 * @param _interval Interval at which the slot should activate.
+	 */
+	Slot(PeriodicScheduler& scheduler, std::chrono::duration _interval);
 
 	/** Callback used to handle timer events.  */
 	void
@@ -64,9 +83,61 @@ namespace crisp
       };
 
 
+      /** Construct a PeriodicScheduler that uses the given `io_service`.
+       *
+       * @param io_service Boost.Asio `io_service` to use.
+       */
+      PeriodicScheduler(boost::asio::io_service& io_service);
+
+      virtual ~PeriodicScheduler();      /**< Destructor. */
+
+
+      /** Schedule a function to be called at regular intervals.
+       *
+       * @param function Function to be called.
+       *
+       * @param interval Interval at which the function is to be called.
+       *
+       * @return Reference to the Action object that represents the scheduled
+       *     function call.
+       */
+      Action&
+      schedule(Action::Function function, std::chrono::duration interval);
+
+
+      /** Reschedule a previously-created Action by moving it into a slot that
+       * matches the given interval.
+       *
+       * @param action The action to be rescheduled.
+       *
+       * @param interval New interval at which the action should be invoked.
+       */
+      void
+      reschedule(Action& action, std::chrono::duration interval);
+
+
+      /** Fetch a reference to the Boost.Asio `io_service` object used by this
+       *  scheduler.
+       *
+       * @return The scheduler's `io_service` reference.
+       */
+      boost::asio::io_service&
+      get_io_service();
+
+
+      /** Cancel an entire slot. */
+      void
+      cancel_slot(Slot& slot);
+
     protected:
-      boost::asio::io_service m_io_service;
-      std::set<Slot> m_slots;
+      /** A mapping from slot-interval to slot.  */
+      typedef std::map<std::chrono::duration,Slot> SlotMap;
+
+      /** Reference to the io_service used by this scheduler. */
+      boost::asio::io_service& m_io_service;
+
+      /** Slots active on this scheduler. */
+      SlotMap m_slots;
     };
   }
 }
