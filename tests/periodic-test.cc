940eb33b20eed09eca6e02fea6fa32532ac45563
Collin J. Sutton <collin.sutton@gmail.com>
2013-11-27 17:47:10 -0800

    tests/periodic-test: Added comments to explain what's going on.

--- tests/periodic-test.cc
+++ tests/periodic-test.cc
@@ -4,11 +4,17 @@
 int
 main(int argc, char* argv[])
 {
+ /* This gives us the user-defined-literal suffix `_Hz`, which takes a value in
+    Hz and transforms it into the interval we need to pass to
+    `PeriodicScheduler::schedule`. */
   using namespace crisp::util::literals;
 
+  /* Instantiate an IO-coordinator object and a scheduler. */
   boost::asio::io_service service;
   crisp::util::PeriodicScheduler scheduler ( service );
 
+
+  /* Schedule some functions to run every so often. */
   scheduler.schedule(1_Hz, [&](crisp::util::PeriodicAction&)
                      { fprintf(stdout, "1\n"); });
   scheduler.schedule(5_Hz, [&](crisp::util::PeriodicAction&)
@@ -16,6 +22,14 @@ main(int argc, char* argv[])
   scheduler.schedule(10_Hz, [&](crisp::util::PeriodicAction&)
                      { fprintf(stdout, "10\n"); });
 
+  /* The timers used by the scheduler won't execute unless there's a thread
+     calling the io_service's `run` method, which parcels out work that results
+     from asynchronous IO calls and other things as long as there's work left
+     to be done.
+
+     Normally we'd have a ProtocolNode running with at least one thread already
+     inside that method; however here the scheduler is the only user of the
+     `io_service` and we need to invoke `run` ourselves. */
   service.run();
 
   return 0;
