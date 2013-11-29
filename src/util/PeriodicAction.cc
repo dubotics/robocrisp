dubotics/robocrisp

commit b7325bc81b77c8ad1064b77bb1d5ecfccb453e87

util/PeriodicAction:  implemented `pause`, `unpause`, `cancel`

src/util/PeriodicAction.cc
@@ -1,4 +1,5 @@
 #include <crisp/util/PeriodicAction.hh>
+#include <crisp/util/PeriodicScheduleSlot.hh>
 
 namespace crisp
 {
@@ -8,16 +9,30 @@ namespace crisp
     PeriodicAction::PeriodicAction(PeriodicScheduleSlot* _slot,
                                    PeriodicAction::Function _function)
       : slot ( _slot ),
-        function ( _function )
+        function ( _function ),
+        active ( true )
     {}
 
     void
     PeriodicAction::timer_expiry_handler(const boost::system::error_code& error)
     {
-      if ( ! error )
+      if ( ( !error ) && action )
         function(*this);
     }
 
+    void
+    PeriodicAction::pause()
+    { active = false; }
+
+    void
+    PeriodicAction::unpause()
+    { active = true; }
+
+    void
+    PeriodicAction::cancel()
+    {
+      slot->remove(*this);
+    }
 
     bool
     PeriodicAction::operator < (const PeriodicAction& action) const

include/crisp/util/PeriodicAction.hh
@@ -29,6 +29,9 @@ namespace crisp
       Function function;	/**< User-defined function to be called by the
 				   slot. */
 
+      bool active;              /**< When `true`, the slot will continue to
+                                   enqueue timer waits on behalf of the action. */
+
       PeriodicAction(PeriodicScheduleSlot* _slot,
                      Function _function);
 
@@ -47,14 +50,17 @@ namespace crisp
       bool
       operator == (const PeriodicAction& action) const;
 
-      /** Temporarily remove the action from its associated slot, preventing
-          it from running. */
+
+      /** Unset the `active` flag, preventing the action's associated slot from
+          enqueuing timer waits on the action. */
       void pause();
 
       /** Unpause an action previously paused with `pause`. */
       void unpause();
 
-      /** Cancel the action, removing it from its slot. */
+      /** Cancel the action, removing it from its slot.  The action object is
+       * destructed and should not be used after the call to this function.
+       */
       void cancel();
     };
 

include/crisp/util/PeriodicScheduleSlot.hh
@@ -98,6 +98,7 @@ namespace crisp
       PeriodicAction&
       emplace(Args... args);
 
+
       /** Remove an action from the slot.
        *
        * @param action The action to be removed.

src/util/PeriodicScheduleSlot.cc
@@ -88,7 +88,8 @@ namespace crisp
 
       /* Set up the action callbacks for this run. */
       for ( PeriodicAction& action : m_actions )
-        m_timer->async_wait(std::bind(&PeriodicAction::timer_expiry_handler, &action, std::placeholders::_1));
+        if ( action.active )
+          m_timer->async_wait(std::bind(&PeriodicAction::timer_expiry_handler, &action, std::placeholders::_1));
 
       /* Set up the slot's expiry handler so we can reset the timer again.  */
       m_timer->async_wait(std::bind(&PeriodicScheduleSlot::timer_expiry_handler, this, std::placeholders::_1));

commit 736700b8abca59c79100df01bb281eff64119fa7

util/PeriodicAction: fixed typo introduced in commit b7325bc

src/util/PeriodicAction.cc
@@ -16,7 +16,7 @@ namespace crisp
     void
     PeriodicAction::timer_expiry_handler(const boost::system::error_code& error)
     {
-      if ( ( !error ) && action )
+      if ( ( !error ) && active )
         function(*this);
     }
 