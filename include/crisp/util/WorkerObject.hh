103a5fcfc09484f2cf862e3df83fe3e7a1e42a46
Collin J. Sutton <collin.sutton@gmail.com>
2013-12-10 17:46:16 -0800

    added crisp::util::WorkerObject: worker-thread manager

---
+++ include/crisp/util/WorkerObject.hh
@@ -0,0 +1,88 @@
+#ifndef crisp_util_WorkerObject_hh
+#define crisp_util_WorkerObject_hh 1
+
+#include <atomic>
+#include <thread>
+#include <cstdint>
+#include <crisp/util/SArray.hh>
+#include <boost/asio/io_service.hpp>
+
+namespace crisp
+{
+  namespace util
+  {
+    /** Provides a convenient way to manage worker threads for Boost.Asio that are attached to a
+     *  specific object.
+     *
+     * Although threads calling the `run` or `run_one` method of a Boost.Asio `io_service`
+     * object may be used for _any_ work that needs to be done (and not necessarily work
+     * produced by the current object), the number of threads required for a distinct set of
+     * objects depends on the design of those objects -- so thread management should be left to
+     * the objects that produce the work.  WorkerObject provides a simple way to manage the
+     * worker threads initiated by a given derived class instance, including clean shutdown.
+     */
+    class WorkerObject
+    {
+    protected:
+      /** Structure used to keep track of and control individual worker threads. */
+      struct WorkerThread
+      {
+        /** Launch the worker thread, servicing the specified `io_service`.
+         *
+         * @param service The Boost.Asio `io_service` on which to service completion handlers.
+         *
+         * @return `true` if the thread was launched, and `false` if it was already running.
+         */
+        bool launch(boost::asio::io_service& service);
+
+        /** Halt the worker thread.  This method will block until the thread has exited.
+         *
+         * @return `true` if the thread could be halted (or was already halted), and `false` if
+         *   the method was called from within the worker thread itself.
+         */
+        bool halt();
+
+        std::thread thread;         /**< Thread handle. */
+        std::atomic<bool> should_halt; /**< Used to indicate to the thread that it should exit. */
+      };
+
+      boost::asio::io_service& m_io_service;
+
+      /** Handles to the worker threads. */
+      crisp::util::SArray<WorkerThread> m_worker_threads;
+
+    public:
+      /** Constructor. */
+      WorkerObject(boost::asio::io_service& _io_service, size_t pool_size);
+      virtual ~WorkerObject();
+
+
+      /** Launch the object's worker threads.
+       *
+       * @return `true` if the threads were launched, and `false` if they were already running.
+       */
+      bool
+      launch();
+
+
+      /** Check if the worker pool can be halted from the calling thread.
+       *
+       * @return `true` if the calling thread is not one of the worker threads.
+       */
+      bool
+      can_halt() const;
+
+
+      /** Halt the object's worker threads.
+       *
+       * @return `true` if the threads were halted or were already halted, and `false` if the
+       *    function was called from the wrong thread.
+       */
+      bool
+      halt();
+    };
+  }
+}
+
+
+#endif  /* crisp_util_WorkerObject_hh */
---
+++ src/util/WorkerObject.cc
@@ -0,0 +1,74 @@
+#include <crisp/util/WorkerObject.hh>
+
+namespace crisp
+{
+  namespace util
+  {
+    bool
+    WorkerObject::WorkerThread::launch(boost::asio::io_service& service)
+    {
+      bool running ( thread.joinable() );
+      if ( ! running )
+        thread = std::thread([&]()
+                             {
+                               while ( ! should_halt )
+                                 service.run_one();
+                             });
+      return ! running;
+    }
+
+    bool WorkerObject::WorkerThread::halt()
+    {
+      if ( thread.get_id() == std::this_thread::get_id() )
+        return false;
+      else if ( thread.joinable() )
+        {
+          should_halt = true;
+          thread.join();
+        }
+
+      return true;
+    }
+
+    
+    WorkerObject::WorkerObject(boost::asio::io_service& _io_service, size_t pool_size)
+      : m_io_service ( _io_service ),
+        m_worker_threads ( pool_size )
+    {}
+
+    WorkerObject::~WorkerObject()
+    {
+      halt();
+    }
+
+    bool
+    WorkerObject::launch()
+    {
+      bool can_launch ( m_worker_threads.size < m_worker_threads.capacity );
+
+      if ( can_launch )
+        for ( size_t i ( m_worker_threads.size ); i < m_worker_threads.capacity; ++i )
+          m_worker_threads.emplace().launch(m_io_service);
+
+      return can_launch;
+    }
+      
+    bool WorkerObject::can_halt() const
+    {
+      for ( const WorkerThread& worker : m_worker_threads )
+        if ( worker.thread.joinable() && worker.thread.get_id() == std::this_thread::get_id() )
+          return false;
+      return true;
+    }
+
+    bool
+    WorkerObject::halt()
+    {
+      bool canhalt ( can_halt() );
+      if ( canhalt )
+        for ( WorkerThread& worker : m_worker_threads )
+          worker.halt();
+      return canhalt;
+    }
+  }
+}
