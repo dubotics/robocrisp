793358ca4f39deda4bcd3a4f8b62b556a0482524
Collin J. Sutton <collin.sutton@gmail.com>
2013-12-17 14:40:53 -0800

    cmake/RoboCRISPConfig.cmake: add libevdev's include dirs if it's avail.

--- cmake/RoboCRISPConfig.cmake
+++ cmake/RoboCRISPConfig.cmake
@@ -1,3 +1,18 @@
 get_filename_component(SELF_DIR "${CMAKE_CURRENT_LIST_FILE}" PATH)
+
+# Include the targets file
 include("${SELF_DIR}/RoboCRISPTargets.cmake")
-get_filename_component(RoboCRISP_INCLUDE_DIRS "${SELF_DIR}/../include/" ABSOLUTE)
+
+# Set up include directories
+get_filename_component(RoboCRISP_INCLUDE_DIR "${SELF_DIR}/../include/" ABSOLUTE)
+
+set(RoboCRISP_INCLUDE_DIRS "${RoboCRISP_INCLUDE_DIR}")
+
+find_package(PkgConfig QUIET)
+pkg_check_modules(LibEvDev QUIET libevdev)
+if(LibEvDev_FOUND)
+  list(APPEND RoboCRISP_INCLUDE_DIRS ${EVDEV_INCLUDE_DIRS})
+endif(LibEvDev_FOUND)
+
+
+set(RoboCRISP_INCLUDE_DIRS "${RoboCRISP_INCLUDE_DIRS}" CACHE STRING "Include directories for RoboCRISP")
