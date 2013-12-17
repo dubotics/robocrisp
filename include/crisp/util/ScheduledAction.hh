dc24bb8a53353937d40ae44c21fa228eb2c39a34
Collin J. Sutton <collin.sutton@gmail.com>
2013-12-17 14:23:40 -0800

    util/Schedule...: force Boost to use std::chrono, part 2
    See also @8e02458, which didn't catch all the places where we needed to
    `#define BOOST_HAS_STD_CHRONO 1`.

--- include/crisp/util/ScheduledAction.hh
+++ include/crisp/util/ScheduledAction.hh
@@ -1,6 +1,7 @@
 #ifndef crisp_util_ScheduledAction_hh
 #define crisp_util_ScheduledAction_hh 1
 
+#define BOOST_ASIO_HAS_STD_CHRONO 1
 #include <boost/asio/steady_timer.hpp>
 #include <functional>
 #include <memory>
