8c57c80c0b0d8f2775ce025fb9a7f2be068e3380
Collin J. Sutton <collin.sutton@gmail.com>
2014-02-20 16:16:22 -0800

    live-test: set max-simultaneous-connections to 1 on the server
    Reminder: connection-count-limiting for number of _simultaneous_
    connections is still unimplemented.

--- tests/live-test.cc
+++ tests/live-test.cc
@@ -103,7 +103,7 @@ main(int argc, char* argv[])
 
   if ( mode == Mode::SERVER )
     {
-      crisp::comms::NodeServer<Node> server ( service, target_endpoint );
+      crisp::comms::NodeServer<Node> server ( service, target_endpoint, 1 );
 
       /* Set up the server's interface configuration with the following test config. */
       using namespace crisp::comms::keywords;
